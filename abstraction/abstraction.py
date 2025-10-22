e13227b4894fffad374ccc348cf0ae46d48eabde
coder7475 <robiulhossain7475@gmail.com>
2025-10-22 08:28:22 +0600

    feat: review abstract method with python

---
+++ abstraction/abstraction.py
@@ -0,0 +1,34 @@
+"""
+Abstraction is the concept of showing only the necessary
+information to the outside world while hiding unnecessary details.
+"""
+
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius**2
+
+
+my_circle = Circle(3)
+
+print(my_circle.area())
